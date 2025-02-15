74fe427339999ed07fdc7de57119d62e3a896222
Link Lin <u7819042@anu.edu.eu>
2025-02-15 16:43:46 +1100

    fix temp

--- src/app/back-office/back-office-header.tsx
+++ src/app/back-office/back-office-header.tsx
@@ -22,8 +22,6 @@ export default function BackOfficeHeader() {
         }        
     };
 
-    useEffect(() => { console.log(data)}, [data])
-
     return <div className="w-full flex flex-row justify-between bg-[#56D4E1] px-3 py-2 text-h5">
         <button className="px-2 rounded-[50px] border-2 border-current">Cancel</button>
         <button className="px-2 rounded-[50px] border-2 border-current"
