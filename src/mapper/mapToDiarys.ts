cd95a5c135e54689440c0a09fe26e3bde0a29b31
Link Lin <u7819042@anu.edu.eu>
2025-02-15 16:25:00 +1100

    hotfix

--- src/mapper/mapToDiarys.ts
+++ src/mapper/mapToDiarys.ts
@@ -1,13 +1,11 @@
 import { Diary } from "@/model/Diary";
-import { Restaurant } from "@/model/Restaurant";
 
 export function mapToDiarys(data: any) {    
     let diarys = [] as Diary[];
     data.diary.forEach((x: any) => {
         diarys.push({
             diaryId: x.diary_id,
-            restaurantId: x.restaurant_id,
-            themeSetting: x.theme_setting
+            restaurantId: x.restaurant_id
        })
     });
     return diarys;
